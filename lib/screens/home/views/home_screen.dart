47545b98a872929bbdbb28c790f821dc8c324f6c
kshah-5683 <khushis8221@gmail.com>
2025-05-26 07:22:43 +0530

    Transactions Screen

--- lib/screens/home/views/home_screen.dart
+++ lib/screens/home/views/home_screen.dart
@@ -1,32 +1,46 @@
 import 'dart:math';
 
+import 'package:expense_tracker/screens/home/stats/stats.dart';
 import 'package:expense_tracker/screens/home/views/main_screen.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int index = 0;
+  late Color selectedItem = Colors.blue;
+  Color unselectedItem = Colors.grey;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      //appBar: AppBar(),
       bottomNavigationBar: ClipRRect(
         borderRadius: BorderRadius.vertical(
           top: Radius.circular(30)
         ),
       child: BottomNavigationBar(
-        backgroundColor: Colors.white,
+        onTap: (value) {
+          setState(() {
+            index = value;
+          });
+        },
+        //backgroundColor: Colors.white,
         showSelectedLabels: false,
         showUnselectedLabels: false,
         elevation: 3,
-        items: const [
+        items:[
           BottomNavigationBarItem(
-            icon: Icon(CupertinoIcons.home),
+            icon: Icon(CupertinoIcons.home, color: index == 0 ? selectedItem : unselectedItem),
             label: 'Home'
           ),
           BottomNavigationBarItem(
-            icon: Icon(CupertinoIcons.graph_square_fill),
+            icon: Icon(CupertinoIcons.graph_square_fill,color: index == 1 ? selectedItem : unselectedItem),
             label: 'Stats'
           )
            
@@ -49,13 +63,15 @@ class HomeScreen extends StatelessWidget {
             ],
             transform: const GradientRotation( pi / 4),
             ),
-        ),
+          ),
         child: const Icon(
           CupertinoIcons.add
-        ),
-      )
+          ),
+        )
       ),
-      body: MainScreen()
-  );
+       body: index == 0 
+              ? MainScreen() 
+              : const StatScreen()
+    );
   }
 }
